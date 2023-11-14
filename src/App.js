a6bb92604f543dfb89552a797ed8d3e2acad6d7d
ixacik <patriklevak@gmail.com>
2023-11-14 13:18:13 +0100

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from "react";
 
-import { Article, Brand, CTA, Feature, Navbar } from "./components";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
